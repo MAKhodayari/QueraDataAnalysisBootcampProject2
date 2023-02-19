2b653ca1633e6247053987ad7db641063de2b94a
mohammadsaleh40 <mohammadsalehmohammadsaleh@gmail.com>
2023-02-19 05:31:51 +0330

    کار کردن روی شبکه عصبی که رگرسیون استفاده می‌کنه.

--- models/TensorFlow/keras.py
+++ models/TensorFlow/keras.py
@@ -5,6 +5,25 @@ import plotly as px
 import numpy as np
 import pandas as pd
 import missingno as msno
+import numpy as np
+import tensorflow as tf
+from tensorflow import keras as K
+# %%
+# 2. define model
+init = K.initializers.RandomUniform(seed=1)
+simple_sgd = K.optimizers.SGD(learning_rate=0.010)#پارامتر نرخ یادگیری اسم جدیدتری دارد
+# توی لینک زیر یک مقدار متفاوت هستش
+# https://keras.io/api/optimizers/sgd/
+# بنظر می‌رسه درستش اینه:
+# simple_sgd = K.optimizers.experimental.SGD(learning_rate=0.1)
+
+# %%
+model = K.models.Sequential()
+model.add(K.layers.Dense(units=10, input_dim=13, kernel_initializer=init, activation='tanh')) # hidden layer
+model.add(K.layers.Dense(units=10, activation='tanh')) # hidden layer
+model.add(K.layers.Dense(units=1, activation=None))
+model.compile(loss='mean_squared_error', optimizer = "adam" , metrics=['mse'])
+
 # %%
 dirname = os.path.dirname(__file__)
 Datasets_path = dirname[:-17] + "Datasets/"
@@ -52,6 +71,20 @@ df.columns[14]
 # %%
 df[df[df.columns[5]].isnull()]
 # اگر توی غیر از اون ستون 'Population Annual Doubling Time (years)' مشکلی می‌بود به کمک این قسمت می‌تونستیم اون بخش‌ها رو پیدا کنیم.
+
+
+# %%
+df.dropna(inplace=True)
+# %%
+df
 # %%
 # الآن دیگه باید وارد فاز طراحی مدل بشیم.
 # من اینجا می‌خوام شبکه عصبی رو تست کنم.
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LinearRegression
+
+train , test = train_test_split(df, test_size=.2, random_state=313)
+train , valid = train_test_split(train, test_size=.2, random_state=313)
+
+
+# %%
